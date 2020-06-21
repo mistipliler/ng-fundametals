59e0e2241c00681307e93546fee65374ef0a45b1
Murat Istipliler <35865164+mistipliler@users.noreply.github.com>
2020-06-21 10:54:15 +0100

    Changed: AppComponent changed as EventsAppComponent

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<h2>Hello World</h2>'
+  selector: 'app-events',
+  template: '<h2>Hello Hello World</h2>'
 })
-export class AppComponent {
+export class EventsAppComponent {
   title = 'app';
 }
