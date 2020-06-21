9442bdb9be213c695d26bac3d50f113c9ef023b9
Murat Istipliler <35865164+mistipliler@users.noreply.github.com>
2020-06-21 11:10:16 +0100

    Update events-app.component.ts

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-events',
-  template: '<h2>Hello Hello World</h2>'
+  template: `
+    <h2>Hello World</h2>
+    <img src="/assets/images/basic-shield.png" />
+  `,
 })
 export class EventsAppComponent {
   title = 'app';
